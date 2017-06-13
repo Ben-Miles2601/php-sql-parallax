e24ad786b96d061d8ca2890cd15a26b9618ea4d5
TBNRBenja <benjamin.p.miles2601@gmail.com>
2017-06-13 16:53:08 +0100

    User adding
    Added the ability to add your own details

--- index.php
+++ index.php
@@ -14,38 +14,46 @@ if ($conn->connect_error) {
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-$sql = "SELECT id, first_name, surname, email_address, telephone FROM friends";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+
+/*if ($result->num_rows > 0) {
 	while ($row = $result->fetch_assoc()) {
 
 	}
 } else {
 	echo "0 results";
-}
+}*/
 
 if(isset($_POST['first_name'])) {
-	echo $_POST['first_name'];
+
+	$sql = "SELECT id, first_name, surname, email_address, telephone FROM friends";
+	$result = $conn->query($sql);
+
+	$sql = "INSERT INTO friends (first_name, surname, email_address, telephone) 
+	VALUES ('" . $_POST['first_name'] . "', '" . $_POST['surname'] . "', '" . $_POST['email_address'] . "', '" . $_POST['telephone'] . "')";
+
+	if ($conn->query($sql) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 	
 }
-//$thing = $_POST[‘first_name’]; {
-//	echo $thing;
+
+//if (isset($_POST['btnSubmit'])) {
+//	SELECT * FROM friends;	
 //}
-//$[$database]->query($pee) {
-//	$pee = “INSERT INTO first_name
-//}
-//<input type="button" value="Say Hi!" onclick="location='test.php'" />
+
 $conn->close();
 ?>
 
 <html>
 	<head>
-		<title> Ben's Dungeon</title>
+		<title>Ben's Dungeon</title>
 	</head>
 	<font color="red">
 	<body background="https://s-media-cache-ak0.pinimg.com/736x/11/d3/15/11d31543d2141800d56c09d93e9c44e3.jpg">
-		<?php echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis, neque vitae blandit auctor, justo ex rutrum mi, et consectetur lectus nulla vitae ipsum. Nam mollis sit amet velit non lobortis. Donec euismod pretium rhoncus. Maecenas elementum tempus nunc nec finibus. Nam semper blandit vulputate. Curabitur at volutpat nibh. Suspendisse dictum facilisis nisl, sed lobortis ligula vulputate a. Aliquam et blandit lectus. Curabitur a tincidunt urna. Donec bibendum erat non nulla elementum fringilla. Maecenas et mollis ipsum. Quisque vitae mi sed odio semper rutrum. In dapibus, mi non mollis lobortis, elit elit vestibulum orci, tincidunt aliquam nulla quam eu dui. Nunc aliquet luctus leo, gravida vestibulum est maximus id.</p>' ?>
+		<?php echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>' ?>
 		<input type="button" value="Say Hi!" onclick="alert('Licky licky')" />
 		</font>
 	</body>
@@ -62,6 +70,11 @@ $conn->close();
 		<input type="text" name="telephone">
 		<button type="submit">Create user</button>
 	</form>
+
+	<h3>List Users?</h3>
+	<input type="submit" id="btnSubmit" value="Retrieve Data" />
+ 
+
 
 		
 	
